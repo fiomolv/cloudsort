3e10fa21540f9c3de077f57c5fe1cfc69d8a66a5
knoernschild <knoernschild@3c72c6fc-d3a7-11de-a505-35228575a32e>
2009-11-18 08:28:45 +0000

    ready to create hits...

--- src/main/java/com/satera/cloudsort/service/CategoryLoader.java
+++ src/main/java/com/satera/cloudsort/service/CategoryLoader.java
@@ -17,8 +17,8 @@ public class CategoryLoader {
     private TurkDAO dao = null;
     
     @Autowired
-    public void setMusicDAO(TurkDAO musicDAO){
-	this.dao = musicDAO;
+    public void setTurkDAO(TurkDAO turkDAO){
+	this.dao = turkDAO;
     }
     
     
