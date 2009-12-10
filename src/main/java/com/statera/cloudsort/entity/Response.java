eed8e0c9a80e6ccc5e855ec9f23f570a5813c727
knoernschild <knoernschild@3c72c6fc-d3a7-11de-a505-35228575a32e>
2009-12-10 01:29:34 +0000

    add WorkerManager

--- src/main/java/com/statera/cloudsort/entity/Response.java
+++ src/main/java/com/statera/cloudsort/entity/Response.java
@@ -16,113 +16,108 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 @Entity
-public class Response implements Serializable
-{
+public class Response implements Serializable {
 
-  private Integer id;
-  private String hitId;
-  private String answer;
-  private String workerId;
-  private String result;
-  private String assignmentId;
-  private Integer requestId;
-  public Integer getRequestId() {
-    return requestId;
-}
+    public static final String RESULT_APPROVED = "APPROVED";
+    public static final String RESULT_REJECTED = "REJECTED";
 
-public void setRequestId(Integer requestId) {
-    this.requestId = requestId;
-}
+    private Integer id;
+    private String hitId;
+    private String answer;
+    private String workerId;
+    private String result;
+    private String assignmentId;
+    private Integer requestId;
 
-@Column(unique=true)
-public String getAssignmentId() {
-    return assignmentId;
-}
+    public Integer getRequestId() {
+	return requestId;
+    }
 
-public void setAssignmentId(String assignmentId) {
-    this.assignmentId = assignmentId;
-}
+    public void setRequestId(Integer requestId) {
+	this.requestId = requestId;
+    }
 
-private Date createdDate;
-  
-  
-  
-@Id
-@GeneratedValue(strategy=GenerationType.AUTO)
-  public Integer getId() {
-    return id;
-}
+    @Column(unique = true)
+    public String getAssignmentId() {
+	return assignmentId;
+    }
 
-public void setId(Integer id) {
-    this.id = id;
-}
+    public void setAssignmentId(String assignmentId) {
+	this.assignmentId = assignmentId;
+    }
 
+    private Date createdDate;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+	return id;
+    }
 
-public String getHitId() {
-    return hitId;
-}
+    public void setId(Integer id) {
+	this.id = id;
+    }
 
-public void setHitId(String hitId) {
-    this.hitId = hitId;
-}
+    public String getHitId() {
+	return hitId;
+    }
 
-@Basic(fetch = FetchType.EAGER)
-public String getAnswer() {
-    return answer;
-}
+    public void setHitId(String hitId) {
+	this.hitId = hitId;
+    }
 
-public void setAnswer(String answer) {
-    this.answer = answer;
-}
+    @Basic(fetch = FetchType.EAGER)
+    public String getAnswer() {
+	return answer;
+    }
 
-public String getWorkerId() {
-    return workerId;
-}
+    public void setAnswer(String answer) {
+	this.answer = answer;
+    }
 
-public void setWorkerId(String workerId) {
-    this.workerId = workerId;
-}
+    public String getWorkerId() {
+	return workerId;
+    }
 
-public String getResult() {
-    return result;
-}
+    public void setWorkerId(String workerId) {
+	this.workerId = workerId;
+    }
 
-public void setResult(String result) {
-    this.result = result;
-}
+    public String getResult() {
+	return result;
+    }
 
-public Date getCreatedDate() {
-    return createdDate;
-}
+    public void setResult(String result) {
+	this.result = result;
+    }
 
-public void setCreatedDate(Date createdDate) {
-    this.createdDate = createdDate;
-}
+    public Date getCreatedDate() {
+	return createdDate;
+    }
 
-public Date getModifiedDate() {
-    return modifiedDate;
-}
+    public void setCreatedDate(Date createdDate) {
+	this.createdDate = createdDate;
+    }
 
-public void setModifiedDate(Date modifiedDate) {
-    this.modifiedDate = modifiedDate;
-}
+    public Date getModifiedDate() {
+	return modifiedDate;
+    }
 
-private Date modifiedDate;
-  
+    public void setModifiedDate(Date modifiedDate) {
+	this.modifiedDate = modifiedDate;
+    }
 
-  public boolean equals(Object obj)
-  {
-    return EqualsBuilder.reflectionEquals(this, obj);
-  }
+    private Date modifiedDate;
 
-  public int hashCode()
-  {
-    return HashCodeBuilder.reflectionHashCode(this);
-  }
-  
-  public String toString()
-  {
-    return ToStringBuilder.reflectionToString(this);
-  }
+    public boolean equals(Object obj) {
+	return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    public int hashCode() {
+	return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    public String toString() {
+	return ToStringBuilder.reflectionToString(this);
+    }
 }
